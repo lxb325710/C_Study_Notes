00f82f6d7fdef596ee6d688bc046ec5da0e5f8dd
xuzhihao <bos@xuzhihao.com>
2018-05-11 10:46:19 +0800

    整理目录

---
+++ Mooc/1_程序设计与C语言/helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+
+int main(){
+
+	printf("helloworld\n");
+
+	/*可以在printf里面直接做运算*/
+	printf("3+2=%d\nd",3+2);
+
+	return 0;
+}
---
+++ Mooc/2_计算/price.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int price = 50 ;
+	int change ;
+	printf("ÇëÊäÈëÆ±Ãæ:\n");
+	scanf("%d",&change);
+	
+	printf("ÕÒÄú:%dÔª",change-50);
+	
+	
+	
+} 
